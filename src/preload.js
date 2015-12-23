608d2b2ead1907f84eae261e3cb76cb83b4c02e6
kamokar <kamokar@wp.pl>
2015-12-23 19:04:16 +0100

    Added Liroy
    Added Liroy sprite - proper animation, height = 48

--- src/preload.js
+++ src/preload.js
@@ -6,7 +6,7 @@ function preload(game) {
     game.load.image(IMAGES.SKY, path + 'sky.png');
     game.load.image(IMAGES.GROUND, path + 'platform.png');
     game.load.image(IMAGES.STAR, path + 'star.png');
-    game.load.spritesheet(IMAGES.PLAYER, path + 'dude.png', 32, 48);
+    game.load.spritesheet(IMAGES.PLAYER, path + 'liroy.png', 32, 48);
     game.load.image(IMAGES.FIREBALL, path + 'fireball.png');
 
       
